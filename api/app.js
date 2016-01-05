05cbc54c85beb0a4361a510dd0c72fffbc95e566
Adam-Greenfield <ajamesgreenfield@gmail.com>
2016-01-05 23:52:41 +0000

    installed method override

--- api/app.js
+++ api/app.js
@@ -1,11 +1,19 @@
 var mongoose = require('mongoose');
 var express = require('express');
+var methodOverride = require("method-override");
 var app = express();
 
 
 
 mongoose.connect('mongodb://localhost:27017/magic_api')
 
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
 
 var routes = require('./config/routes');
 app.use(routes);
