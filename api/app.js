9d75830a8c5146fe5bb1b022850edc39de820c13
Adam-Greenfield <ajamesgreenfield@gmail.com>
2016-01-05 22:44:24 +0000

    set up mongoose conect

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
 
+mongoose.connect('mongodb://localhost:27017/magic_api')
 app.listen(3000, function(){
   console.log("Yes Mother");
 });
