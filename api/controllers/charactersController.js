edd764a3bfe0fba48724e730cc11d33e7e34fdf5
Adam-Greenfield <ajamesgreenfield@gmail.com>
2016-01-08 20:03:37 +0000

    added index and show to campaign and char controller

--- api/controllers/charactersController.js
+++ api/controllers/charactersController.js
@@ -1,3 +1,16 @@
 var Character = require('../models/Character.js');
 
+function characterIndex(req, res){
+  Character.find(function(err, characters){
+    if (err) return res.status(404).json({message: 'Something went wrong and we could not pull the characters.'});
+    res.status(200).json({ characters: characters });
+  });
+};
+
+function characterShow(req, res){
+  Character.findById(req.params.id, function(err, character){
+    if (err) return res.status(404).json({message: 'Something went wrong and we could not find the character.'});
+    res.status(200).json({ character: character });
+  });
+};
 
