1d1c02fdd005400cc3e0a412700249f852b9f01b
Adam-Greenfield <ajamesgreenfield@gmail.com>
2016-01-08 18:02:13 +0000

    wrote statement preventing users from being in the campaign twice

--- api/models/Campaign.js
+++ api/models/Campaign.js
@@ -8,4 +8,31 @@ var campaignSchema = new mongoose.Schema({
   players: [{type: mongoose.Schema.ObjectId, ref: 'User'}],
 });
 
+campaignSchema.pre("save", function(next){
+
+  var campaign = this;
+
+  if(campaign.isModified("players")){
+    var uniquePlayers = [];
+    campaign.players.forEach(function(player){
+      if(uniquePlayers.indexOf(player) === -1){
+        uniquePlayers.push(player);
+      }
+    });
+    campaign.players = uniquePlayers;
+  }
+
+  if(campaign.isModified("admin_users")){
+    var uniqueAdmin_userss = [];
+    campaign.players.forEach(function(admin_users){
+      if(uniquePlayers.indexOf(admin_users) === -1){
+        uniquePlayers.push(admin_users);
+      }
+    });
+    campaign.admin_users = uniqueAdmin_users;
+  }
+
+  next();
+});
+
 module.exports = mongoose.model("Campaign", campaignSchema);
